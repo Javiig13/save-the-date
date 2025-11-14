a189a6541e43b7a2aa296de4620c282e34a48b28
Javiig13 <javieriglesiaslopez13@gmail.com>
2025-11-14 15:36:52 +0100

    build: rutas robustas en scripts de minificación

--- build/minify-css.js
+++ build/minify-css.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 const fs = require('fs');
+const path = require('path');
 const csso = require('csso');
-const input = fs.readFileSync('../styles.css','utf8');
+const root = path.resolve(__dirname, '..');
+const inputPath = path.join(root, 'styles.css');
+const outputPath = path.join(root, 'styles.min.css');
+const input = fs.readFileSync(inputPath, 'utf8');
 const { css } = csso.minify(input, { restructure: true });
-fs.writeFileSync('../styles.min.css', css);
+fs.writeFileSync(outputPath, css);
 console.log('CSS minificado -> styles.min.css');
--- build/minify-js.js
+++ build/minify-js.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 const fs = require('fs');
+const path = require('path');
 const terser = require('terser');
-const input = fs.readFileSync('../script.js','utf8');
+const root = path.resolve(__dirname, '..');
+const inputPath = path.join(root, 'script.js');
+const outputPath = path.join(root, 'script.min.js');
 (async () => {
+  const input = fs.readFileSync(inputPath, 'utf8');
   const result = await terser.minify(input, { compress: true, mangle: true });
-  fs.writeFileSync('../script.min.js', result.code);
+  fs.writeFileSync(outputPath, result.code);
   console.log('JS minificado -> script.min.js');
 })();
