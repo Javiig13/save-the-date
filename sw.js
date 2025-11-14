0d454217462ea89456ed13fd9ea3630b07b1fa95
Javiig13 <javieriglesiaslopez13@gmail.com>
2025-11-14 15:51:15 +0100

    feat: favicon HP + minificados referenciados

--- sw.js
+++ sw.js
@@ -2,9 +2,10 @@ const CACHE = 'std-cache-v2';
 const ASSETS = [
   './',
   './index.html',
-  './styles.css',
-  './script.js',
-  './config.js'
+  './styles.min.css',
+  './script.min.js',
+  './config.js',
+  './favicon.svg'
 ];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting()));
